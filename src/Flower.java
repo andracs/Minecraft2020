414f1773e3aee155a5be3f76512d366f4a0b540d
MMadsen99 <math9082@edu.easj.dk>
2020-10-02 13:10:12 +0200

    Lavet Flower

--- src/Flower.java
+++ src/Flower.java
@@ -6,4 +6,30 @@
  */
 
 public class Flower {
+
+    private String flowerType;  //Type - 1 red 2 blue 3 yellow 4 white
+    private String skin = "flower.png";
+    private boolean canBreak = true;
+    private boolean canStack = true;
+    private boolean canGrow = true;
+    private boolean canBurn = true;
+    private boolean needsGrass = true;
+
+    Flower() {
+        System.out.println(Game.visNytObjekt("wood"));
+    }
+
+    public String getFlowerType() {
+        return flowerType;
+    }
+
+    public void setFlowerType(String flowerType) {
+        // Her kan jeg indbygge noget sikkerhed
+        if (flowerType.equals("red") || flowerType.equals("blue") || flowerType.equals("yellow") || flowerType.equals("white"))
+        {this.flowerType = flowerType;}
+        else {
+            System.out.println("Jeg kan bedre lide røde blomster");
+            setFlowerType("red");
+        }
+    }
 }
