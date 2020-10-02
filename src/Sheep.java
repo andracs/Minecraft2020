ea5106043abd2911be87e99b67781bc2ef56c1d0
Delalx <Delal-kesen@outlook.com>
2020-10-02 14:26:25 +0200

    Updated sheep

--- src/Sheep.java
+++ src/Sheep.java
@@ -13,19 +13,53 @@ public class Sheep {
     int health = 10;
     int damageTaken = 0;
     boolean damage = false;
+    boolean movingFront = false;
+    boolean movingBack = false;
+    boolean movingRight = false;
+    boolean movingLeft = false;
+    boolean jumping = false;
 
     Sheep() {
         System.out.println(Game.visNytObjekt("sheep"));
     }
 
     private int healthRemaining(int currentHealth) {
-        if (damage = true) {
+        if (damage == true) {
             health = currentHealth - damageTaken;
         }
         else {
             health = currentHealth;
         }
         return health;
+    }
+
+    void MoveForward(int positionX) {
+        while (movingFront == true) {
+            positionX++;
+        }
+    }
+    void MoveBackwards(int positionX) {
+        while (movingBack == true) {
+            positionX--;
+        }
+    }
+    void MoveRight (int positionZ) {
+        while (movingRight == true) {
+            positionZ++;
+        }
+    }
+    void MoveLeft (int positionZ) {
+        while (movingLeft == true) {
+            positionZ++;
+        }
+    }
+    void Jump (int positionY) {
+        while (jumping == true) {
+            positionY++;
+        }
+        for (int i = positionY; i < 0; i--) {
+            positionY--;
+        }
     }
 
 
